d7be64e95297cbe481289680643e5a62ae286a14
adenohitu <49752462+adenohitu@users.noreply.github.com>
2025-06-29 04:15:31 +0900

    fix: Site image

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -73,9 +73,9 @@ const config: Config = {
     // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: "My Site",
+      title: "MockLay",
       logo: {
-        alt: "My Site Logo",
+        alt: "MockLay Logo",
         src: "img/logo.svg",
       },
       items: [
